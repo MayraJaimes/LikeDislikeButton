2f5026f9a6438393457ad0a1a723b3e2eb58d17e
Mayra Jaimes <mtj26@georgetown.edu>
2020-09-15 15:07:28 -0500

    added the function to increase and decrease count

--- src/components/LikeDislikeButton.js
+++ src/components/LikeDislikeButton.js
@@ -2,24 +2,57 @@ import React, { useState } from 'react'
 
 const LikeDislikeButton = () => {
 
+  const styles = {
+    likeButton: {
+      width: "200px",
+      color: "red",
+      margin: "10px",
+      padding: "10px"
+    },
+    disLikeButton: {
+      width: "200px",
+      color: "blue",
+      margin: "10px",
+      padding: "10px"
+    }
+  }
+
   const [likeCount, setLikeCount] = useState(5)
+  const [clickedLike, setClickedLike] = useState(false)
+
   const [disLikeCount, setDisLikeCount] = useState(8)
+  const [clickedDisLike, setClickedDisLike] = useState(false)
 
 
   const increaseCount = () => {
-    setLikeCount(prevCount => prevCount + 1)
+    if(clickedLike === false){
+      setLikeCount(prevCount => prevCount + 1)
+      setClickedLike(true)
+    }
+    else {
+      setLikeCount(prevCount => prevCount - 1)
+      setClickedLike(false)
+    }
   }
 
   const decreaseCount = () => {
     if(disLikeCount > 0){
-      setDisLikeCount(prevCount => prevCount - 1)
+
+      if(clickedDisLike === false){
+        setDisLikeCount(prevCount => prevCount - 1)
+        setClickedDisLike(true);
+      }
+      else{
+        setDisLikeCount(prevCount => prevCount + 1)
+        setClickedDisLike(false);
+      }
     }
   }
 
   return(
     <div>
-     <button onClick={increaseCount}>{likeCount}</button>
-     <button onClick={decreaseCount}>{disLikeCount}</button>
+     <button style={styles.likeButton} onClick={increaseCount}> Like | {likeCount}</button>
+     <button style={styles.disLikeButton} onClick={decreaseCount}> Dislike | {disLikeCount}</button>
 
     </div>
   ) 
